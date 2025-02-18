4b596d022084217cbc585f74bcbb42a6fe2bf18d
MinChatr <min_etc@hotmail.com>
2025-02-18 20:45:14 +0700

    Try to summit to lab 3

--- src/server.ts
+++ src/server.ts
@@ -18,7 +18,7 @@
 //  }
  
 //  function addEvent(newEvent : Event) : Event { 
-//    newEvent.id = events.length +1; 
+//    newEvent.id = events.length 1; 
 //    events.push(newEvent); 
 //    return newEvent; 
 //  }
@@ -57,34 +57,129 @@
 //   console.log(`App listening at http : //localhost:${port}`);
 // });
 
-// Task 2 Lab3 ------------------------------------------------------
-import express, { Request, Response } from "express";
-import add from "./functions";
-import type { Event } from "./service/eventService";
-import { addEvent, getAllEvents, getEventByCategory, getEventByID } from "./service/eventService";
+// // Task 2 Lab3 ------------------------------------------------------
+// import express, { Request, Response } from "express";
+// import type { Event } from "./service/eventService";
+// import { addEvent, getAllEvents, getEventByCategory, getEventByID } from "./service/eventService";
+// // import { addEvent, getAllEvents as allEvent, getEventByCategory, getEventByID } from "./service/eventService"; // as เป็นการเปลี่ยนชื่อ function ที่ import มา
 
+// import add from "./functions";
+// const app = express();
+// app.use(express.json());
+// const port = 3002;
+
+//  app.get("/", (req: Request, res: Response) => {
+//   res.send(getAllEvents());
+//   res.send(add(5, 3)); // เอามาใส่เฉยๆ ให้เห็นว่าใส่ function ได้
+// });
+
+//  app.get("/events", (req,res) => { 
+//    if (req.query.category){ 
+//      const category = req.query.category;
+//      const filteredEvents = getEventByCategory(category as string); 
+//      res.json(filteredEvents);
+//    } else { 
+//      res.json(getAllEvents); 
+//    }
+//  })
+ 
+//  app.get("/events/:id", (req,res)=>{ 
+//    const id = parseInt(req.params.id); 
+//    const event = getEventByID(id); 
+//    if (event){ 
+//      res.json(event); 
+//    } else { 
+//      res.status(404).send("Event not found"); 
+//    }
+//  })
+ 
+//  app.post("/events", (req,res)=>{ 
+//    const newEvent : Event = req.body; 
+//    addEvent(newEvent); 
+//    res.json(newEvent); 
+//  }); 
+
+//  app.listen(port, () => {
+//   console.log(`App listening at http : //localhost:${port}`);
+// });
+ 
+// Task 3 Lab3 ------------------------------------------------------
+// import express, { Request, Response } from "express";
+// import type { Event } from "./service/eventService";
+// import { addEvent, getAllEvents, getEventByCategory, getEventByID } from "./service/eventService";
+// // import { addEvent, getAllEvents as allEvent, getEventByCategory, getEventByID } from "./service/eventService"; // as เป็นการเปลี่ยนชื่อ function ที่ import มา
+
+// import add from "./functions";
+// const app = express();
+// app.use(express.json());
+// const port = 3002;
+
+//  app.get("/", (req: Request, res: Response) => {
+//   res.send(getAllEvents());
+//   res.send(add(5, 3)); // เอามาใส่เฉยๆ ให้เห็นว่าใส่ function ได้
+// });
+
+//  app.get("/events", async (req,res) => { 
+//    if (req.query.category){ 
+//      const category = req.query.category;
+//      const filteredEvents = await getEventByCategory(category as string); 
+//      res.json(filteredEvents);
+//    } else { 
+//      res.json(await getAllEvents); 
+//    }
+//  })
+ 
+//  app.get("/events/:id", async (req,res)=>{ 
+//    const id = parseInt(req.params.id); 
+//    const event = await getEventByID(id); 
+//    if (event){ 
+//      res.json(event); 
+//    } else { 
+//      res.status(404).send("Event not found"); 
+//    }
+//  })
+ 
+//  app.post("/events", async (req,res)=>{ 
+//    const newEvent : Event = req.body; 
+//    await addEvent(newEvent); 
+//    res.json(newEvent); 
+//  }); 
+
+//  app.listen(port, () => {
+//   console.log(`App listening at http : //localhost:${port}`);
+// });
+ 
+// Task 4 Lab3 ------------------------------------------------------
+import express, { Request, Response } from "express";
+import type { Event } from "./models/event";
+import { addEvent, getAllEvents, getEventByCategory, getEventById } from "./repository/eventRepositoryDb";
+
+import add from "./functions";
 const app = express();
 app.use(express.json());
 const port = 3002;
 
- app.get("/", (req: Request, res: Response) => {
-  res.send(getAllEvents());
+ app.get("/", async (req: Request, res: Response) => {
+  res.send(await getAllEvents());
+});
+
+ app.get("/add", (req: Request, res: Response) => {
   res.send(add(5, 3)); // เอามาใส่เฉยๆ ให้เห็นว่าใส่ function ได้
 });
 
- app.get("/events", (req,res) => { 
+ app.get("/events", async (req,res) => { 
    if (req.query.category){ 
      const category = req.query.category;
-     const filteredEvents = getEventByCategory(category as string); 
+     const filteredEvents = await getEventByCategory(category as string); 
      res.json(filteredEvents);
    } else { 
-     res.json(getAllEvents); 
+     res.json(await getAllEvents()); 
    }
  })
  
- app.get("/events/:id", (req,res)=>{ 
+ app.get("/events/:id", async (req,res)=>{ 
    const id = parseInt(req.params.id); 
-   const event = getEventByID(id); 
+   const event = await getEventById(id); 
    if (event){ 
      res.json(event); 
    } else { 
@@ -92,9 +187,9 @@ const port = 3002;
    }
  })
  
- app.post("/events", (req,res)=>{ 
+ app.post("/events", async (req,res)=>{ 
    const newEvent : Event = req.body; 
-   addEvent(newEvent); 
+   await addEvent(newEvent); 
    res.json(newEvent); 
  }); 
 
@@ -102,5 +197,25 @@ const port = 3002;
   console.log(`App listening at http : //localhost:${port}`);
 });
  
+
+import multer from 'multer';
+import { uploadFile } from './service/uploadFileService';
+
+const upload = multer({ storage: multer.memoryStorage() }); 
+app.post('/upload', upload.single('file'), async (req: any, res: any) => { 
+  try { 
+ 
+const file = req.file; 
+if (!file) { 
+return res.status(400).send('No file uploaded.'); 
+} 
+const bucket = 'image'; 
+const filePath = `uploads/${file.originalname}`; 
+await uploadFile(bucket, filePath, file); 
+res.status(200).send('File uploaded successfully.'); 
+  } catch (error) { 
+    res.status(500).send('Error uploading file.'); 
+  } 
+}); 
 
 
--- src/service/eventService.ts
+++ src/service/eventService.ts
@@ -1,100 +1,242 @@
-export function getEventByCategory(category:string) : Event[] {
-  const filteredEvents = events.filter((event) => event.category === category); 
-  return filteredEvents;
- }
- 
-export function getAllEvents() : Event[]{
-   return events; 
- }
- 
-export function getEventByID(id : number) : Event | undefined { 
-   return events.find((event) => event.id === id); 
- }
- 
- export function addEvent(newEvent : Event) : Event { 
-   newEvent.id = events.length +1; 
-   events.push(newEvent); 
-   return newEvent; 
- }
+// // Task 2 Lab3 ------------------------------------------------------
+// export function getEventByCategory(category:string) : Event[] {
+//   const filteredEvents = events.filter((event) => event.category === category);
+//   return filteredEvents;
+//  }
 
-export interface Event {
-    id: number;
-    category: string;
-    title: string;
-    decription: string;
-    location: string;
-    date: string;
-    time: string;
-    petsAllowed: boolean;
-    organizer: string;
-  }
-  
-const events : Event[] = [
-    {
-      id: 1,
-      category: "Music",
-      title: "Concert",
-      decription: "A live concert",
-      location: "London",
-      date: "2021-07-01",
-      time: "19:00",
-      petsAllowed: false,
-      organizer: "Live Nation",
-    },
-    {
-      id: 2,
-      category: "Music",
-      title: "Festival",
-      decription: "A music festival",
-      location: "Manchester",
-      date: "2021-07-15",
-      time: "12:00",
-      petsAllowed: true,
-      organizer: "Festival Republic",
-    },
-    {
-      id: 3,
-      category: "Music",
-      title: "Gig",
-      decription: "A gig",
-      location: "Birmingham",
-      date: "2021-07-30",
-      time: "20:00",
-      petsAllowed: true,
-      organizer: "Academy Music Group",
-    },
-    {
-      id: 4,
-      category: "Sports",
-      title: "Football Match",
-      decription: "A live football match",
-      location: "London",
-      date: "2021-07-01",
-      time: "19:00",
-      petsAllowed: false,
-      organizer: "Premier League",
-    },
-    {
-      id: 5,
-      category: "Music",
-      title: "Festival",
-      decription: "A music festival",
-      location: "Manchester",
-      date: "2021-07-15",
-      time: "12:00",
-      petsAllowed: true,
-      organizer: "Festival Republic",
-    },
-    {
-      id: 6,
-      category: "Music",
-      title: "Gig",
-      decription: "A gig",
-      location: "Birmingham",
-      date: "2021-07-30",
-      time: "20:00",
-      petsAllowed: true,
-      organizer: "Academy Music Group",
-    },
-  ];
-  
+// export function getAllEvents() : Event[]{
+//    return events;
+//  }
+
+// export function getEventByID(id : number) : Event | undefined {
+//    return events.find((event) => event.id === id);
+//  }
+
+//  export function addEvent(newEvent : Event) : Event {
+//    newEvent.id = events.length +1;
+//    events.push(newEvent);
+//    return newEvent;
+//  }
+
+// export interface Event {
+//     id: number;
+//     category: string;
+//     title: string;
+//     decription: string;
+//     location: string;
+//     date: string;
+//     time: string;
+//     petsAllowed: boolean;
+//     organizer: string;
+//   }
+
+// const events : Event[] = [
+//     {
+//       id: 1,
+//       category: "Music",
+//       title: "Concert",
+//       decription: "A live concert",
+//       location: "London",
+//       date: "2021-07-01",
+//       time: "19:00",
+//       petsAllowed: false,
+//       organizer: "Live Nation",
+//     },
+//     {
+//       id: 2,
+//       category: "Music",
+//       title: "Festival",
+//       decription: "A music festival",
+//       location: "Manchester",
+//       date: "2021-07-15",
+//       time: "12:00",
+//       petsAllowed: true,
+//       organizer: "Festival Republic",
+//     },
+//     {
+//       id: 3,
+//       category: "Music",
+//       title: "Gig",
+//       decription: "A gig",
+//       location: "Birmingham",
+//       date: "2021-07-30",
+//       time: "20:00",
+//       petsAllowed: true,
+//       organizer: "Academy Music Group",
+//     },
+//     {
+//       id: 4,
+//       category: "Sports",
+//       title: "Football Match",
+//       decription: "A live football match",
+//       location: "London",
+//       date: "2021-07-01",
+//       time: "19:00",
+//       petsAllowed: false,
+//       organizer: "Premier League",
+//     },
+//     {
+//       id: 5,
+//       category: "Music",
+//       title: "Festival",
+//       decription: "A music festival",
+//       location: "Manchester",
+//       date: "2021-07-15",
+//       time: "12:00",
+//       petsAllowed: true,
+//       organizer: "Festival Republic",
+//     },
+//     {
+//       id: 6,
+//       category: "Music",
+//       title: "Gig",
+//       decription: "A gig",
+//       location: "Birmingham",
+//       date: "2021-07-30",
+//       time: "20:00",
+//       petsAllowed: true,
+//       organizer: "Academy Music Group",
+//     },
+//   ];
+
+// Task 3 Lab3 ------------------------------------------------------
+// export function getEventByCategory(category:string) : Promise<Event[]> {
+//   const filteredEvents = events.filter((event) => event.category === category);
+//   return Promise.resolve(filteredEvents);
+//  }
+
+// export function getAllEvents() : Promise<Event[]>{
+//    return Promise.resolve(events);
+//  }
+
+// export function getEventByID(id : number) : Promise<Event| undefined> {
+//    return Promise.resolve(events.find((event) => event.id === id));
+//  }
+
+//  export function addEvent(newEvent : Event) : Promise<Event> {
+//    newEvent.id = events.length +1;
+//    events.push(newEvent);
+//    return Promise.resolve(newEvent);
+//  }
+
+// export interface Event {
+//     id: number;
+//     category: string;
+//     title: string;
+//     decription: string;
+//     location: string;
+//     date: string;
+//     time: string;
+//     petsAllowed: boolean;
+//     organizer: string;
+//   }
+
+// const events : Event[] = [
+//     {
+//       id: 1,
+//       category: "Music",
+//       title: "Concert",
+//       decription: "A live concert",
+//       location: "London",
+//       date: "2021-07-01",
+//       time: "19:00",
+//       petsAllowed: false,
+//       organizer: "Live Nation",
+//     },
+//     {
+//       id: 2,
+//       category: "Music",
+//       title: "Festival",
+//       decription: "A music festival",
+//       location: "Manchester",
+//       date: "2021-07-15",
+//       time: "12:00",
+//       petsAllowed: true,
+//       organizer: "Festival Republic",
+//     },
+//     {
+//       id: 3,
+//       category: "Music",
+//       title: "Gig",
+//       decription: "A gig",
+//       location: "Birmingham",
+//       date: "2021-07-30",
+//       time: "20:00",
+//       petsAllowed: true,
+//       organizer: "Academy Music Group",
+//     },
+//     {
+//       id: 4,
+//       category: "Sports",
+//       title: "Football Match",
+//       decription: "A live football match",
+//       location: "London",
+//       date: "2021-07-01",
+//       time: "19:00",
+//       petsAllowed: false,
+//       organizer: "Premier League",
+//     },
+//     {
+//       id: 5,
+//       category: "Music",
+//       title: "Festival",
+//       decription: "A music festival",
+//       location: "Manchester",
+//       date: "2021-07-15",
+//       time: "12:00",
+//       petsAllowed: true,
+//       organizer: "Festival Republic",
+//     },
+//     {
+//       id: 6,
+//       category: "Music",
+//       title: "Gig",
+//       decription: "A gig",
+//       location: "Birmingham",
+//       date: "2021-07-30",
+//       time: "20:00",
+//       petsAllowed: true,
+//       organizer: "Academy Music Group",
+//     },
+//   ];
+
+// Task 4 Lab3 ------------------------------------------------------
+// import type { Event } from "../models/event";
+// import {
+//     addEvent as addNewEvent,
+//     getAllEvents as allEvents,
+//     getEventByID as eventById
+// } from "../repository/eventRepository";
+
+// export function getEventByCategory(category: string): Promise<Event[]> {
+//   return getEventByCategory(category);
+// }
+
+// export function getAllEvents(): Promise<Event[]> {
+//   return allEvents();
+// }
+
+// export function getEventById(id: number): Promise<Event | undefined> {
+//   return eventById(id);
+// }
+
+// export function addEvent(newEvent: Event): Promise<Event> {
+//  return addNewEvent(newEvent);
+// }
+import type { Event } from "../models/event";
+// import * as repo from "../repository/eventRepository";
+import * as repo from "../repository/eventRepositoryDb";
+
+export function getEventByCategory(category: string): Promise<Event[]> {
+  return repo.getEventByCategory(category);
+}
+export function getAllEvents(): Promise<Event[]> {
+  return repo.getAllEvents();
+}
+export function getEventById(id: number): Promise<Event | undefined> {
+  return repo.getEventById(id);
+}
+export function addEvent(newEvent: Event): Promise<Event> {
+  return repo.addEvent(newEvent);
+}
+
