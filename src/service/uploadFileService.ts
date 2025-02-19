99489263fda6f120fa67a72052ec4d79b0d591ae
MinChatr <min_etc@hotmail.com>
2025-02-19 10:14:30 +0700

    Task 10

--- src/service/uploadFileService.ts
+++ src/service/uploadFileService.ts
@@ -1,3 +1,29 @@
+// Task 9 lab 3
+// import { PutObjectCommand } from "@aws-sdk/client-s3";
+// import { s3Client } from '../awsConfig';
+
+// export async function uploadFile(
+//   bucket: string,
+//   filePath: string,
+//   file: Express.Multer.File
+// ): Promise<void> {
+
+//   const params = {
+//     Bucket: bucket,
+//     Key: filePath,
+//     Body: file.buffer,
+//     ContentType: file.mimetype,
+//   };
+
+//   try {
+//     const data = await s3Client.send(new PutObjectCommand(params));
+//     console.log("File uploaded successfully:", data);
+//   } catch (error) {
+//     console.error("Error uploading file:", error);
+//   }
+// }
+
+// Task 10 lab 3
 import { PutObjectCommand } from "@aws-sdk/client-s3";
 import { s3Client } from '../awsConfig';
 
@@ -5,20 +31,24 @@ export async function uploadFile(
   bucket: string,
   filePath: string,
   file: Express.Multer.File
-): Promise<void> {
-  
+): Promise<string> {
+
   const params = {
     Bucket: bucket,
     Key: filePath,
     Body: file.buffer,
     ContentType: file.mimetype,
   };
-
-  try {
-    const data = await s3Client.send(new PutObjectCommand(params));
-    console.log("File uploaded successfully:", data);
+   
+  try { 
+  const data = await s3Client.send(new PutObjectCommand(params)); 
+  console.log('File uploaded successfully:', data); 
+  const publicUrl = `https://kdkwuxvoydubcwehmxfy.supabase.co/storage/v1/object/public/images/${filePath}`; 
+  console.log('File uploaded successfully:', publicUrl);  
+  return publicUrl; 
   } catch (error) {
-    console.error("Error uploading file:", error);
+    console.error('Error uploading file:', error); 
+    return 'error'; 
   }
 }
 
